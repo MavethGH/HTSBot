ae18922a688901e47611abd506b6769278247ada
MavethGH <65792235+MavethGH@users.noreply.github.com>
2020-12-24 16:23:59 -0600

    Fix a stupid syntax error opening the config file

--- bot/bot.py
+++ bot/bot.py
@@ -8,5 +8,5 @@ class HTSBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        with open('config.json') as config:
+        with open('bot/config.json', 'r') as config:
             self.config = json.load(config)
