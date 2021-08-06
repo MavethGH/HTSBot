9101ed182abf482ea19caf63be914651fba863c0
Maveth <maveth1@pm.me>
2021-08-06 12:14:00 -0500

    Added a regex to test if user is a spambot

--- bot/cogs/is_bot.py
+++ bot/cogs/is_bot.py
@@ -1,5 +1,15 @@
+import re
+
+
 def is_bot(user):
     """Checks if user is likely a spambot"""
-    
+
+    # Will eventually be a score based on similarities to other spambots
+    isbot = False
+
+    spambot_re = re.compile(r"^[A-Z]{1}.*[0-9]{1}$")
+    if spambot_re.match(user.name):
+        isbot = True
+
     # For now, assume that all users being tested are bots
-    return True
+    return isbot
