2bb6fe816196b902f56f6b1ab08951728a8658e7
Maveth <maveth1@pm.me>
2021-08-06 12:24:16 -0500

    More bot checks

--- bot/cogs/is_bot.py
+++ bot/cogs/is_bot.py
@@ -5,11 +5,19 @@ def is_bot(user):
     """Checks if user is likely a spambot"""
 
     # Will eventually be a score based on similarities to other spambots
-    isbot = False
+    isbot = True
 
+    # Matches the auto-generated names of spambots we get
     spambot_re = re.compile(r"^[A-Z]{1}.*[0-9]{1}$")
-    if spambot_re.match(user.name):
-        isbot = True
+    if not spambot_re.match(user.name):
+        isbot = False
 
-    # For now, assume that all users being tested are bots
+    # Spambots don't change their avatars
+    if user.avatar_url != user.default_avatar_url:
+        isbot = False
+
+    # Spambots don't change their nicknames
+    if user.nick != user.name:
+        isbot = False
+
     return isbot
